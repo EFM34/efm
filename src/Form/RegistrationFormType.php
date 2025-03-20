86b3948130d6162781f264f97457818c93791fb5
Ensemble Frédéric Mistral <ensamblefredericmistral@gmail.com>
2025-03-20 13:04:08 +0100

    Modification de .env

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,16 +4,17 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class RegistrationFormType extends AbstractType
 {
@@ -55,46 +56,59 @@ class RegistrationFormType extends AbstractType
                 ]
             ])
             ->add('plainPassword', RepeatedType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'type' => PasswordType::class,
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ])
-                ],
-                'first_options' => [
-                    'label' => false,
-                    'attr' => [
-                        'placeholder' => 'Votre mot de passe ...',
-                        'class' => 'form-control'
+                    // instead of being set onto the object directly,
+                    // this is read and encoded in the controller
+                    'type' => PasswordType::class,
+                    'mapped' => false,
+                    'attr' => ['autocomplete' => 'new-password'],
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Veuillez entrer un mot de passe.',
+                        ]),
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => 'Votre mot de passe doit comporter au moins {{ limit }} caractères.',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 4096,
+                        ])
                     ],
-                    'row_attr' => [
-                        'class' => 'form-group mb-3'
-                    ]
-                ],
-                'second_options' => [
-                    'label' => false,
-                    'attr' => [
-                        'placeholder' => 'Confirme votre mot de passe ...',
-                        'class' => 'form-control'
+                    'first_options' => [
+                        'label' => false,
+                        'attr' => [
+                            'placeholder' => 'Entre votre mot de passe',
+                            'class' => 'form-control'
+                        ],
+                        'row_attr' => [
+                            'class' => 'form-group mb-3'
+                        ]
                     ],
-                    'row_attr' => [
-                        'class' => 'form-group mb-3'
+                    'second_options' => [
+                        'label' => false,
+                        'attr' => [
+                            'placeholder' => 'Confirmation mot de passe',
+                            'class' => 'form-control'
+                        ],
+                        'row_attr' => [
+                            'class' => 'form-group mb-3'
+                        ]
                     ]
-                ] 
-        ])
-    ;
-}
+                ]
+            )
+            // ->add('register', SubmitType::class, [
+            //     "label" => "Insciption",
+            //     "attr" => [
+            //         "placeholder" => "Insciption",
+            //         "class" => "btn btn-fill-out btn-block",
+            //         "name" => "register",
+            //         "type" => "submit",
+            //     ],
+            //     "row_attr" => [
+            //         "class" => "col-md-12"
+            //     ],
 
+            // ])
+        ;
+    } 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
